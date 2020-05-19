bd3a7f4e67b6fdc9bb84ecd4491524ec68d51a7d
TENEr98 <levon.tarayan.98@mail.ru>
2020-05-19 19:53:50 +0500

    Refactor tests and ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -1,59 +1,65 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
+
 from rest_framework import status
 from rest_framework.test import APIClient
+
 from core.models import Ingredient
+
 from recipe.serializers import IngredientSerializer
 
 
-INGREDIENT_URL = reverse('recipe:ingredient-list')
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
-class PublicIngredientApiTests(TestCase):
-    """Test the public available ingredients API"""
+class PublicIngredientsApiTests(TestCase):
+    """Test the publicly available ingredients API"""
 
     def setUp(self):
         self.client = APIClient()
 
     def test_login_required(self):
         """Test that login is required to access the endpoint"""
-        res = self.client.get(INGREDIENT_URL)
+        res = self.client.get(INGREDIENTS_URL)
+
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateIngredientsApiTests(TestCase):
-    """Test the private ingredients API """
+    """Test the private ingredients API"""
 
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
-            'test@gmail.com',
+            'test@londonappdev.com',
             'testpass'
         )
         self.client.force_authenticate(self.user)
 
     def test_retrieve_ingredient_list(self):
         """Test retrieving a list of ingredients"""
-
         Ingredient.objects.create(user=self.user, name='Kale')
         Ingredient.objects.create(user=self.user, name='Salt')
-        res = self.client.get(INGREDIENT_URL)
-        ingredients = Ingredient.objects.all().order_by('-name')
 
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
         serializer = IngredientSerializer(ingredients, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
-    def test_ingredient_limited_to_user(self):
-        """Test that ingredients for the authenticated user are returned"""
+    def test_ingredients_limited_to_user(self):
+        """Test that ingredients for the authenticated user are returend"""
         user2 = get_user_model().objects.create_user(
-            'other@gmail.com',
+            'other@londonappdev.com',
             'testpass'
         )
         Ingredient.objects.create(user=user2, name='Vinegar')
         ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
-        res = self.client.get(INGREDIENT_URL)
+
+        res = self.client.get(INGREDIENTS_URL)
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
@@ -61,18 +67,17 @@ class PrivateIngredientsApiTests(TestCase):
     def test_create_ingredient_successful(self):
         """Test create a new ingredient"""
         payload = {'name': 'Cabbage'}
-        self.client.post(INGREDIENT_URL, payload)
+        self.client.post(INGREDIENTS_URL, payload)
 
         exists = Ingredient.objects.filter(
             user=self.user,
-            name=payload['name']
+            name=payload['name'],
         ).exists()
         self.assertTrue(exists)
 
     def test_create_ingredient_invalid(self):
         """Test creating invalid ingredient fails"""
-        payload = {
-            'name': ''
-        }
-        res = self.client.post(INGREDIENT_URL, payload)
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,43 +1,37 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+
 from core.models import Tag, Ingredient
+
 from recipe import serializers
 
 
-class TagViewSet(
-                viewsets.GenericViewSet,
-                mixins.CreateModelMixin,
-                mixins.ListModelMixin):
-    """Manage tags in the database"""
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for user owned recipe attributes"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        return self.queryset.filter(
+            user=self.request.user
+        ).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new tag"""
+        """Create a new object"""
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(
-                        viewsets.GenericViewSet,
-                        mixins.CreateModelMixin,
-                        mixins.ListModelMixin):
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new ingredient"""
-        serializer.save(user=self.request.user)
