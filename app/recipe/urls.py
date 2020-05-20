27e14fc4bfcc65c0acbad17a49144e1e8f547939
TENEr98 <levon.tarayan.98@mail.ru>
2020-05-20 14:25:15 +0500

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipe', views.RecipeViewSet)
 app_name = 'recipe'
 urlpatterns = [
     path('', include(router.urls))
