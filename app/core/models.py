a0f8118c164507321c16f98cf7c0cc2fccf49c57
TENEr98 <levon.tarayan.98@mail.ru>
2020-05-22 01:49:26 +0500

    Added recipe image upload feature

--- app/core/models.py
+++ app/core/models.py
@@ -13,6 +13,7 @@ def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image"""
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
+
     return os.path.join('uploads/recipe/', filename)
 
 
